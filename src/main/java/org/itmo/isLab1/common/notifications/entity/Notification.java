ebb46877559bf4a45bd9c612c5582c973da95421
t.i.kaparulin <t.i.kaparulin@tbank.ru>
2026-01-17 10:11:24 +0300

    add Notification entity

---
+++ src/main/java/org/itmo/isLab1/common/notifications/entity/Notification.java
@@ -0,0 +1,49 @@
+package org.itmo.isLab1.common.notifications.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.itmo.isLab1.common.entity.BaseEntity;
+import org.itmo.isLab1.users.User;
+import org.itmo.isLab1.utils.datetime.ZonedDateTimeConverter;
+
+import java.time.ZonedDateTime;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "art2art_notifications")
+public class Notification implements BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "art2art_notifications_id_seq")
+    @SequenceGenerator(name = "art2art_notifications_id_seq", sequenceName = "art2art_notifications_id_seq", allocationSize = 1)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @NotBlank
+    @Column(name = "message", nullable = false, columnDefinition = "TEXT")
+    private String message;
+
+    @Column(name = "link", columnDefinition = "TEXT")
+    private String link;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "category", nullable = false, length = 50)
+    private NotificationCategory category;
+
+    @Column(name = "read_at")
+    @Convert(converter = ZonedDateTimeConverter.class)
+    private ZonedDateTime readAt;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    @Convert(converter = ZonedDateTimeConverter.class)
+    private ZonedDateTime createdAt;
+}
